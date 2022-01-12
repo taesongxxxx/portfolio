f502a4d0dad585b4664ef1ef36dd8c7bba721343
taesong <82091824+taesongxxxx@users.noreply.github.com>
2022-01-12 22:07:43 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarheight = navbar.getBoundingClientRect().height;
@@ -18,13 +17,19 @@ const navbar__menu = document.querySelector('.navbar__menu');
 document.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
-  
   if (link == null) {
     return;
   } 
 
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({behavior:"smooth"});
-  
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+const homecontainer = document.querySelector('.home');
+document.addEventListener('scroll', () => {
+  homecontainer.style.opacity = (homeHeight - window.scrollY) / homeHeight;
+});
